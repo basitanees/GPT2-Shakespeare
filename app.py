2534f4ae811b73e906f03e4907193d517ece696d
Abdul Basit Anees <50206820+basitanees@users.noreply.github.com>
2023-03-22 03:03:16 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from starlette.middleware.cors import CORSMiddleware
+from typing import List
+import torch
+from utils import get_model, get_tokenizer
+from params import SPECIAL_TOKENS, MAXLEN
+
+tokenizer = get_tokenizer(special_tokens=SPECIAL_TOKENS)
+model = get_model(tokenizer, 
+                  special_tokens=SPECIAL_TOKENS,
+                  load_model_path='pytorch_model.bin'
+                 )
+model.eval()
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cpu')
+
+
+app = FastAPI(title="Shakespeare")
+
+# allow CORS requests from any host so that the JavaScript can communicate with the server
+app.add_middleware(
+    CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
+
+class Request(BaseModel):
+    text: str    
+      
+class AllModelsResponse(BaseModel):
+    output: List[str]
+
+
+@app.post("/predict", response_model=AllModelsResponse)
+async def predict(request: Request):
+    prompt = SPECIAL_TOKENS['bos_token'] + request.text
+    generated = torch.tensor(tokenizer.encode(prompt)).unsqueeze(0)
+    generated = generated.to(device)
+    
+    output = []
+    # Top-p (nucleus) text generation (10 samples):
+    sample_outputs = model.generate(generated, 
+                                    do_sample=True,   
+                                    min_length=50, 
+                                    max_length=MAXLEN,
+                                    top_k=30,                                 
+                                    top_p=0.7,        
+                                    temperature=0.9,
+                                    repetition_penalty=2.0,
+                                    num_return_sequences=10
+                                    )
+
+    for i, sample_output in enumerate(sample_outputs):
+        text = tokenizer.decode(sample_output, skip_special_tokens=True)
+        output.append(text)
+    
+    response = AllModelsResponse(output=output)
+    return response
---
+++ params.py
@@ -0,0 +1,34 @@
+DEBUG           = False
+
+INPUT_DIR       = 'articles'
+
+USE_APEX        = True
+APEX_OPT_LEVEL  = 'O1'
+
+MODEL           = 'gpt2' #{gpt2, gpt2-medium, gpt2-large, gpt2-xl}
+
+UNFREEZE_LAST_N = 6 #The last N layers to unfreeze for training
+
+SPECIAL_TOKENS  = { "bos_token": "<|BOS|>",
+                    "eos_token": "<|EOS|>",
+                    "unk_token": "<|UNK|>",                    
+                    "pad_token": "<|PAD|>",
+                    "sep_token": "<|SEP|>"}
+                    
+MAXLEN          = 768  #{768, 1024, 1280, 1600}
+
+TRAIN_SIZE      = 0.8
+
+if USE_APEX:
+    TRAIN_BATCHSIZE = 4
+    BATCH_UPDATE    = 16
+else:
+    TRAIN_BATCHSIZE = 2
+    BATCH_UPDATE    = 32
+
+EPOCHS          = 4
+LR              = 5e-4
+EPS             = 1e-8
+WARMUP_STEPS    = 1e2
+
+SEED            = 2020
